f59ca4a62c7914a430b16095523d07937bca4ca8
Gavin Bish <gavinbish12@icloud.com>
2025-12-30 12:39:01 -0500

    a
    Signed-off-by: Gavin Bish <gavinbish12@icloud.com>

---
+++ src/mui-custom.d.ts
@@ -0,0 +1,14 @@
+﻿import '@mui/material/styles';
+import { PaletteColorOptions } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    tertiary: Palette['primary'];
+    accent?: Palette['primary'];
+  }
+  
+  interface PaletteOptions {
+    tertiary?: PaletteColorOptions;
+    accent?: PaletteColorOptions;
+  }
+}
